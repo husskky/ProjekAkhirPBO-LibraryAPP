86919b467dc7dc9041186e3537c81b43819b6069
husskky <89946765+husskky@users.noreply.github.com>
2024-11-24 00:22:53 +0700

    Add files via upload

---
+++ src/MgrPerpustakaan/MainAdmin.java
@@ -0,0 +1,19 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MgrPerpustakaan;
+
+/**
+ *
+ * @author rrenj1
+ */
+public class MainAdmin {
+        public static void main(String[] args) {
+
+        LoginAdmin LoginFrame = new LoginAdmin();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+}
